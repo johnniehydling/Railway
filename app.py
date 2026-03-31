23ccecfca4261449dd9af4606dd7e28ec139e845
johnniehydling <johnnie.hydling@gmail.com>
2026-03-31 12:18:08 +0200

    Implement push notification on '/answer' route
    Added functionality to send a push notification when the '/answer' route is accessed.

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, Response, jsonify
+from flask import Flask, jsonify
 import os
+import requests
 
 app = Flask(__name__)
+
+PUSHOVER_TOKEN = "ajgbvbmr51ash8b4xbf2qpxx3ei28o"
+PUSHOVER_USER = "u9cmpck4su5zv28x9se3qtw9d6vd8g"
+
+def send_push():
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": PUSHOVER_TOKEN,
+            "user": PUSHOVER_USER,
+            "message": "Dörren har öppnats! 🚪",
+            "title": "Porttelefon"
+        }
+    )
 
 @app.route('/')
 def health():
@@ -9,10 +24,8 @@ def health():
 
 @app.route('/answer', methods=['POST'])
 def answer():
-    response = {
-        "play": "sound/dtmf/5"
-    }
-    return jsonify(response)
+    send_push()
+    return jsonify({"play": "sound/dtmf/5"})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
